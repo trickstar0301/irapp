e1d8f2a8fdb3f875eae561f384dc5b17ffaebfac
trickstar0301 <tomida.ldp@gmail.com>
2016-09-08 02:30:37 +0900

    create

---
+++ app.js
@@ -0,0 +1,41 @@
+// node-modules
+var app = require('koa')();
+var route = require('koa-route');
+var serve = require('koa-static');
+var views = require('koa-views');
+var request = require('request');
+const exec = require('child_process').exec;
+var bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
+
+// ectをテンプレートエンジンとして指定
+app.use(views(__dirname + '/views', {
+  map: {
+    html: 'ect'
+  }
+}));
+
+// GET /remocon
+app.use(route.get('/remocon', function *(){	
+	yield this.render('remocon.ect', {	
+    title: 'Remote controller', 
+  });
+}));
+
+// POST /ir
+app.use(route.post('/ir', function *(){
+	console.log('body: ' + JSON.stringify(this.request.body));
+	var target = JSON.stringify(this.request.body.file)
+	exec('sudo ./sendir fan_volume.dat 1 1', (err, stdout, stderr) => {
+	//callback処理
+	if (err) { console.log(err); }
+		console.log(stdout);
+	});
+	this.body=true;
+}));
+
+// static files
+app.use(serve(__dirname + '/public'));
+
+app.listen(3000, function () { console.log('app listening on port 3000!'); });
